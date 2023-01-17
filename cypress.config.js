69fa6d71259bca0a116cc1468f4f409937020268
chopradhruv6038 <chopradhruv6038@gmail.com>
2023-01-17 20:51:38 +0400

    added mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '8g7a59',
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
 
     watchForFileChanges: false,
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
